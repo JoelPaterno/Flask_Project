08f0256b05d83e8ca9ab42ee6ed0c71aeaf2cc7a
joelpaterno <joelpaterno1@gmail.com>
2024-08-23 17:11:42 +1000

    added html template

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}"
 
-@app.route("/admin")
+@app.route("/admin/")
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name="Admin!"))
 
 if  __name__ == "__main__":
     app.run()
